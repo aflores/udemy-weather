45483d5c233068af7e0ba23bc0ae8a4ae2237c41
Armando Flores <aflores57@gmail.com>
2016-09-07 21:05:26 -0400

    end of lecture 57 before hooking middleware

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = '714c691859c6dda7c06869617659015a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+//action types
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// action creator
+export function fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);  // this will return a promise
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
---
+++ src/containers/search-bar.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { term: ''}
+
+    // bindings
+    this.onInputChange = this.onInputChange.bind(this)
+    this.onSubmitForm = this.onSubmitForm.bind(this)
+  }
+
+  onInputChange(event) {
+    // console.log(event.target.value)
+    this.setState({term: event.target.value})
+  }
+
+  onSubmitForm(event) {
+    event.preventDefault()
+
+    // go get the data
+    this.props.fetchWeather(this.state.term)
+    this.setState({ term: '' })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitForm} className="input-group">
+        <input 
+          placeholder="Get a five-day forecast for your favorite city"
+          className= "form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators( { fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
